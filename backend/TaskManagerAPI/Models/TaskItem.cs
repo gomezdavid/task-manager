5e22ea0d9493056c139413cf84cbc0298784c0df
gomezdavid <davidalejandrogomez2002@gmail.com>
2026-03-16 20:28:26 -0300

    add backend folder

---
+++ backend/TaskManagerAPI/Models/TaskItem.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManagerAPI.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Priority { get; set; }
+        public bool IsCompleted { get; set; }
+
+    }
+}
